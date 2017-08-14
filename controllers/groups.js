8ade4295a3d8c7325d2c2c026095f79d9f871d41
nikkifargus <nikki@fargus.co.uk>
2017-08-14 12:08:03 +0100

    Added groups model and controller

---
+++ controllers/groups.js
@@ -0,0 +1,66 @@
+const Group = require('../models/group');
+
+function groupIndexRoute(req, res){
+  Group
+    .find()
+    .exec()
+    .then(groups => res.status(200).json(groups))
+    .catch(err => res.status(500).json(err));
+}
+
+function groupCreateRoute(req, res){
+  Group
+    .create(req.body)
+    .then(group => res.status(201).json(group))
+    .catch(err => res.status(500).json(err));
+}
+
+function groupShowRoute(req, res){
+  Group
+    .findById(req.params.id)
+    .exec()
+    .then(group => {
+      if(!group) return res.status(404).json({ message: 'no group found'});
+
+      return res.status(200).json(group);
+    })
+    .catch(err => res.status(500).json(err));
+}
+
+function groupUpdateRoute(req, res){
+  Group
+    .findById(req.params.id)
+    .exec()
+    .then(group => {
+      if(!group) return res.status(404).json({ message: 'no group found'});
+
+      for(const field in req.body ){
+        group[field] = req.body[field];
+      }
+
+      return group.save();
+    })
+    .then(group => res.status(200).json(group))
+    .catch(err => res.status(500).json(err));
+}
+
+function groupDeleteRoute(req, res){
+  Group
+    .findById(req.params.id)
+    .exec()
+    .then(group => {
+      if(!group) return res.status(404).json({ message: 'no group found'});
+
+      return group.remove();
+    })
+    .then(()=> res.status(204).end())
+    .catch(err => res.status(500).json(err));
+}
+
+module.exports = {
+  index: groupIndexRoute,
+  create: groupCreateRoute,
+  show: groupShowRoute,
+  update: groupUpdateRoute,
+  delete: groupDeleteRoute
+};
---
+++ models/group.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  body: String,
+  user: { type: mongoose.Schema.ObjectId, ref: 'User'}
+});
+
+const destinationsSchema = new mongoose.Schema({
+  destinationName: String,
+  destinationType: String,
+  destinationInfo: String,
+  destinationLink: String,
+  //how is lat/lon done for googleplaces
+  destinationLatLong: {lat: Number , long: Number}
+});
+
+const groupSchema = new mongoose.Schema({
+  title: { type: String, required: true, unique: true },
+  image: { type: String, required: true },
+  dates: {type: Date, required: true},
+  createdBy: {type: mongoose.Shcema.ObjectId, ref: 'User'},
+  //how do we create members array when they join 'do'
+  members: [{type: mongoose.Shcema.ObjectId, ref: 'User'}],
+  destinations: [destinationsSchema],
+  comments: [commentSchema],
+  //how to populate this automatically
+  stagOrHen: {type: Boolean, required: true}
+
+});
+
+module.exports = mongoose.model('Group', groupSchema);
