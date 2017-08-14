e2f6557ed3ef620d0c011d289237f260f9fc07d8
nikkifargus <nikki@fargus.co.uk>
2017-08-14 16:24:47 +0100

    added new ctrl view

---
+++ src/js/controllers/groups/groups-new.controller.js
@@ -0,0 +1,17 @@
+angular
+.module('project3')
+.controller('GroupsNewCtrl', GroupsNewCtrl);
+
+GroupsNewCtrl.$inject = ['Group', '$state'];
+function GroupsNewCtrl(Group, $state){
+  const vm = this;
+
+  vm.create = groupsCreate;
+
+  function groupsCreate(){
+    Group
+    .save(vm.group)
+    .$promise
+    .then(()=> $state.go('groupsIndex'));
+  }
+}
