28331c682702470f42742e15cc66302e7620539e
nikkifargus <nikki@fargus.co.uk>
2017-08-17 19:48:52 +0100

    styled index

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -1,10 +1,18 @@
 angular
-  .module('project3')
-  .controller('MainCtrl', MainCtrl);
+.module('project3')
+.controller('MainCtrl', MainCtrl);
 
 MainCtrl.$inject = ['CurrentUserService', '$rootScope', '$state'];
 function MainCtrl(CurrentUserService, $rootScope, $state) {
   const vm = this;
+
+  $rootScope.$on('$stateChangeSuccess', ()=> {
+    // vm.hideNav = false;
+    
+    // if new state === 'home'
+      // vm.hideNav = true;
+  });
+
   $rootScope.$on('loggedIn', () => {
     vm.user = CurrentUserService.currentUser;
 
